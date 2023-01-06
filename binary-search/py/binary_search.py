56481033212f03dbd73870c37d4749a99ad0ede7
vov-a <vova.open.to.work@gmail.com>
2023-01-06 16:25:14 +0300

    add recursive bin search in python

--- binary-search/py/binary_search.py
+++ binary-search/py/binary_search.py
@@ -14,3 +14,17 @@ def binary_search(list, item):
     
   return None
 
+
+def binary_search_r(list, item, left, right):
+  
+  if left <= right:
+    mid = (left + right) // 2
+    guess = list[mid]
+    if guess == item:
+      return mid
+    if item < guess:
+      return binary_search_r(list, item, left, mid - 1)
+    else:
+      return binary_search_r(list, item, mid + 1, right)
+  else:  
+    return None
--- binary-search/py/test_binary_search.py
+++ binary-search/py/test_binary_search.py
@@ -1,5 +1,5 @@
 import pytest
-from binary_search import binary_search
+from binary_search import binary_search, binary_search_r
 
 
 @pytest.mark.parametrize("list, item, expected", 
@@ -8,6 +8,7 @@ from binary_search import binary_search
    ([-1, 2, 3, 4, 5, 6], 3, 2)])
 def test_binary_search_positive(list, item,  expected):
    assert binary_search(list, item) == expected
+   assert binary_search_r(list, item, 0, len(list)-1) == expected
 
 
 def test_binary_search_negative():
